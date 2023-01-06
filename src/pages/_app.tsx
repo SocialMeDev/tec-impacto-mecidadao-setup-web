7f51796fd1c51ec6c4e073ee6b9a1ed76cf694cf
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-06 15:20:51 -0300

    feat: foi instalado e colocado a font Poppins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+import 'configs/fonts'
+
 import customTheme from 'styles'
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,13 @@
 import { extendTheme, withDefaultSize } from '@chakra-ui/react'
 
-const customTheme = extendTheme({}, withDefaultSize({ size: 'sm' }))
+const customTheme = extendTheme(
+  {
+    fonts: {
+      heading: 'Poppins',
+      body: 'Poppins'
+    }
+  },
+  withDefaultSize({ size: 'sm' })
+)
 
 export default customTheme
