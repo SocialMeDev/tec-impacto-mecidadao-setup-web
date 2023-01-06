88f6e1eaf3803dc43f3814531b3e597c75d6b36d
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-06 00:24:31 -0300

    refactor: movendo a pasta pages para dentro de src

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Setup() {
+  return (
+    <>
+      <Head>
+        <title>Setup</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Setup</h1>
+      </main>
+    </>
+  )
+}
