4d00d6206d5e5a9e1c11b86eb08efeb0d24dc7b9
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-01 17:23:32 -0300

    feat: foi criado o protocolo AddressBody

---
+++ src/lib/protocols/address/AddressBody.ts
@@ -0,0 +1,11 @@
+export type AddressBody = {
+  address?: string
+  address_neighbourhood?: string
+  address_number?: string
+  address_city?: string
+  address_uf?: string
+  address_country?: string
+  address_zip_code?: string
+  address_lat?: string
+  address_lng?: string
+}
