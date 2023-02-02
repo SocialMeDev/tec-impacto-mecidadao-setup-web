5aa87c9b76c686adee3b26888eddb028e0e321d4
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-02 16:36:27 -0300

    feat: foi criado o iconi ArrowRight

---
+++ src/lib/assets/icons/Arrows/ArrowRight/index.tsx
@@ -0,0 +1,29 @@
+import { FC, memo } from 'react'
+import { createIcon, IconProps } from '@chakra-ui/icons'
+
+import { Icon } from 'lib/components'
+
+const ArrowRightIcon = createIcon({
+  displayName: 'ArrowRight',
+  viewBox: '0 0 24 24',
+  path: (
+    <>
+      <path
+        xmlns="http://www.w3.org/2000/svg"
+        d="M10,16l4,-4l-4,-4"
+        strokeLinecap="round"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        fill="none"
+        strokeLinejoin="round"
+      />
+      <path xmlns="http://www.w3.org/2000/svg" fill="none" d="M0,0h24v24h-24Z" />
+    </>
+  ),
+})
+
+const ArrowRight: FC<IconProps> = (props) => {
+  return <Icon as={ArrowRightIcon} {...props} />
+}
+
+export default memo(ArrowRight)
