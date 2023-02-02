442367e798d6d440d1e1d79618b99f1fbb72eb40
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-02 04:06:14 -0300

    feat: foi criado a função diferenceBetweenDates

---
+++ src/lib/utils/transforms/date/diferenceBetweenDates/index.ts
@@ -0,0 +1,31 @@
+import {
+  differenceInMilliseconds,
+  differenceInDays,
+  parseISO,
+  differenceInSeconds,
+  differenceInMinutes,
+} from 'date-fns'
+
+type PossibleReturnsTypes = {
+  [key: string]: (dateLeft: Date | number, dateRight: Date | number) => void
+}
+
+type ReturnType = 'days' | 'milliseconds' | 'seconds' | 'minutes'
+
+export function diferenceBetweenDates(startDate: string, endDate: string, returnedBy: ReturnType = 'days') {
+  if (!startDate || !endDate) return
+
+  const startDateParse = parseISO(startDate)
+  const endDateParse = parseISO(endDate)
+
+  const possibleReturnsTypes: PossibleReturnsTypes = {
+    days: differenceInDays,
+    minutes: differenceInMinutes,
+    seconds: differenceInSeconds,
+    milliseconds: differenceInMilliseconds,
+  }
+
+  const differenceInChoosedType = possibleReturnsTypes[returnedBy]
+
+  return differenceInChoosedType(endDateParse, startDateParse)
+}
