dd46d28ccc04550030dd4f5d5a4bbd4255fad768
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-03 20:42:34 -0300

    refactor: mudando a tipagem dos parâmetros removeEverythingThatIsNotNumber

--- src/lib/utils/transforms/string/removeEverythingThatIsNotNumber/index.ts
+++ src/lib/utils/transforms/string/removeEverythingThatIsNotNumber/index.ts
@@ -1,4 +1,4 @@
-export function removeEverythingThatIsNotNumber(value: string, defaultEmptyValue = ''): string {
+export function removeEverythingThatIsNotNumber(value: string | undefined, defaultEmptyValue = ''): string {
   if (!value) return defaultEmptyValue
 
   return value.replace(/[^\d]+/g, '')
