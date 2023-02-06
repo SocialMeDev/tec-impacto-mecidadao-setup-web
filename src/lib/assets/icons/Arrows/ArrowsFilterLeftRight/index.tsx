936229e20282e3eb599c9d815373dfa6d6383bcf
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-06 01:39:07 -0300

    feat: foi criado o iconi ArrowsFilterLeftRight

---
+++ src/lib/assets/icons/Arrows/ArrowsFilterLeftRight/index.tsx
@@ -0,0 +1,33 @@
+import { FC, memo } from 'react'
+import { createIcon, IconProps } from '@chakra-ui/icons'
+
+import { Icon } from 'lib/components'
+
+const ArrowsFilterLeftRightIcon = createIcon({
+  displayName: 'ArrowsFilterLeftRightIcon',
+  viewBox: '0 0 24 24',
+  path: (
+    <>
+      <g
+        xmlns="http://www.w3.org/2000/svg"
+        strokeLinecap="round"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        fill="none"
+        strokeLinejoin="round"
+      >
+        <path d="M16.103,10.793l2.897,-2.896l-2.897,-2.897" />
+        <path d="M5,7.9h14" />
+        <path d="M7.897,13.207l-2.897,2.896l2.897,2.897" />
+        <path d="M19,16.1h-14" />
+      </g>
+      <path xmlns="http://www.w3.org/2000/svg" fill="none" d="M0,0h24v24h-24Z" />
+    </>
+  ),
+})
+
+const ArrowsFilterLeftRight: FC<IconProps> = (props) => {
+  return <Icon as={ArrowsFilterLeftRightIcon} {...props} />
+}
+
+export default memo(ArrowsFilterLeftRight)
--- src/lib/assets/icons/Arrows/index.ts
+++ src/lib/assets/icons/Arrows/index.ts
@@ -1,3 +1,4 @@
 export { default as ArrowFilterUpDown } from './ArrowFilterUpDown'
 export { default as ArrowRight } from './ArrowRight'
 export { default as ArrowLeft } from './ArrowLeft'
+export { default as ArrowsFilterLeftRight } from './ArrowsFilterLeftRight'
