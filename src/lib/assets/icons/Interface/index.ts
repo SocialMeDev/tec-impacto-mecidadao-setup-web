9ae9bec18a844511d28876c0027c68d17410d713
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-06 01:58:18 -0300

    feat: foi criado o iconi ZoomMinus

--- src/lib/assets/icons/Interface/index.ts
+++ src/lib/assets/icons/Interface/index.ts
@@ -1,3 +1,4 @@
 export { default as CloudUpload } from './CloudUpload'
 export { default as Trash } from './Trash'
+export { default as ZoomMinus } from './ZoomMinus'
 export { default as ZoomPlus } from './ZoomPlus'
