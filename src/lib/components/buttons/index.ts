e13b23ed4c2de9a7b1771bbe711d0d875e424207
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-06 15:47:03 -0300

    feat: foi criado o componente UpdateButton

--- src/lib/components/buttons/index.ts
+++ src/lib/components/buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as BindButton } from './BindButton'
 export { default as CreateButton } from './CreateButton'
 export { default as DeleteButton } from './DeleteButton'
+export { default as UpdateButton } from './UpdateButton'
