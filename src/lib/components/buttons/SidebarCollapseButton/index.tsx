4c0ec555a85a6da44e01847b2f05a5d903d2828d
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-06 22:37:03 -0300

    feat: foi criado o componente SidebarCollapseButton

---
+++ src/lib/components/buttons/SidebarCollapseButton/index.tsx
@@ -0,0 +1,20 @@
+import { FC, memo } from 'react'
+import { IconButton, IconButtonProps } from '@chakra-ui/react'
+
+import { MenuArrowSquare } from '@lib/assets/icons'
+import { useSidebar } from '@lib/contexts/Sidebar/Provider'
+
+const SidebarCollapseButton: FC<IconButtonProps> = (props) => {
+  const { isOpen, onOpen, onClose } = useSidebar()
+
+  return (
+    <IconButton
+      variant="ghost"
+      onClick={isOpen ? onClose : onOpen}
+      icon={<MenuArrowSquare transform={isOpen ? 'rotate(180deg)' : 'rotate(0deg)'} transition="0.5s all" />}
+      {...props}
+    />
+  )
+}
+
+export default memo(SidebarCollapseButton)
