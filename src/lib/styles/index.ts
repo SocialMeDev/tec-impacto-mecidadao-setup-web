a63a9fb5733c69fb94421a034ad20f1849f2c3bc
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-07 00:42:21 -0300

    feat: foi adicionado as fonts no tema do Chakra

--- src/lib/styles/index.ts
+++ src/lib/styles/index.ts
@@ -1,10 +1,12 @@
 import { extendTheme, withDefaultColorScheme, withDefaultSize } from '@chakra-ui/react'
 
+import fonts from './fonts'
 import global from './global'
 import semanticTokens from './semanticTokens'
 
 const customTheme = extendTheme(
   {
+    fonts,
     semanticTokens,
     styles: {
       global,
