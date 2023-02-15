6ec09dd4f1d6b01f9685a4fb5230a63c8cf17741
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-15 19:47:55 -0300

    refactor: mudando a linguage do HTML

--- src/lib/assets/icons/Weather/index.ts
+++ src/lib/assets/icons/Weather/index.ts
@@ -1,2 +1,4 @@
-export { default as Moon } from './Moon'
-export { default as Sun } from './Sun'
+export { default as MoonOutline } from './MoonOutline'
+export { default as MoonSolid } from './MoonSolid'
+export { default as SunOutline } from './SunOutline'
+export { default as SunSolid } from './SunSolid'
