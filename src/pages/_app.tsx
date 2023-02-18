c4abf04217f425d5af9295b332eb39b7eeae00fd
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-02-18 19:07:46 -0300

    refactor: modificando o _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,9 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
-import 'lib/config/fonts'
-import 'lib/config/styles'
-
-import createCustomTheme from '@lib/styles/createCustomTheme.factory'
-
 export default function App({ Component, pageProps }: AppProps) {
-  const theme = createCustomTheme()
-
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
   )
