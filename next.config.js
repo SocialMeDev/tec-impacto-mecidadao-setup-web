c005c614a69dabe6119d2ca0856932bf9e555a8c
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-03-28 16:27:44 -0300

    feat: adicionando os exemplos de variáveis de ambiente

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    APP_KEY: process.env.APP_KEY,
+    INSTITUTION_SLUG: process.env.INSTITUTION_SLUG,
+    INSTITUTION_API_URL: process.env.INSTITUTION_API_URL,
+  },
 }
 
 module.exports = nextConfig
